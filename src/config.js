8465f7796cf0292e6d3b9628845827586f59340a
michaelcodes1993 <lpskirito7@outlook.com>
2020-08-22 17:05:29 -0400

    fixed error

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    PORT:process.env.PORT || 80,
-    NODE_ENV:process.env.NODE_ENV || development
+    PORT: process.env.PORT || 80,
+    NODE_ENV: process.env.NODE_ENV || "development"
 }
